f0b7e94f5965001ff414210a2023539b7ae6b327
malikljalid <181765794+malikljalid@users.noreply.github.com>
2026-07-08 19:42:48 +0100

    First Functional Version of the Project
    Refactored

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
-let container = document.querySelector(".container");
+const   GRID_PIXEL_WIDTH = 480;
+const   GRID_DIVS_MAX    = 100;
 
+const   container   = document.querySelector(".container");
+const   button      = document.querySelector("button");
 
-let grid = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,],
-];
+let     gridDivs = 16;
+let     grid = Array.from({ length: GRID_DIVS_MAX }, () => Array(GRID_DIVS_MAX));
 
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        grid[i][j] = document.createElement("div");
-        grid[i][j].classList.add("square");
-        container.appendChild(grid[i][j]);
+function gridInit(gridDivs) {
+    for (let i = 0; i < gridDivs; i++) {
+        for (let j = 0; j < gridDivs; j++) {
+            grid[i][j] = document.createElement("div");
+            grid[i][j].classList.add("square");
+            container.appendChild(grid[i][j]);
+        }
     }
 }
 
-const squares = document.querySelectorAll(".square");
+function gridClear(gridDivs) {
+    for (let i = 0; i < gridDivs; i++) {
+        for (let j = 0; j < gridDivs; j++) {
+            container.removeChild(grid[i][j]);
+        }
+    }
+}
 
-squares.forEach( (square) => square.addEventListener("mousemove", () => { square.classList.add("checked"); }));
+function setNewSquaresSize(square) {
+    square.style.width  = String(GRID_PIXEL_WIDTH / gridDivs) + "px";
+    square.style.height = String(GRID_PIXEL_WIDTH / gridDivs) + "px";
+}
+
+function gridDraw() {
+    const squares = document.querySelectorAll(".square");
+
+    squares.forEach( (square) => { setNewSquaresSize(square); } );
+    squares.forEach( (square) => square.addEventListener("mouseenter", () => { square.classList.add("checked"); }));
+}
+
+gridInit(gridDivs);
+gridDraw();
+
+button.addEventListener("click", () => {
+    gridClear(gridDivs);
+    gridDivs = parseInt(prompt("Enter the new grid size : "));
+    gridInit(gridDivs);
+    gridDraw();
+});
